9ff02776d9050274ec6ba8995259c7e137b88e82
Angel-leg <asolorzanoangel@gmail.com>
2025-10-31 12:57:52 -0600

    Actualización AuthService.js

--- frontend/src/auth/AuthService.js
+++ frontend/src/auth/AuthService.js
@@ -3,8 +3,8 @@ import { UserManager } from 'oidc-client-ts';
 const config = {
   authority: 'https://cognito-idp.us-east-1.amazonaws.com/us-east-1_iheBfIEVe',
   client_id: '18dqsc4m66ii15jcfbsabkv6s',
-  redirect_uri: 'http://localhost:4200/auth/callback',
-  post_logout_redirect_uri: 'http://localhost:4200/',
+  redirect_uri: '${baseUrl}/auth/callback',
+  post_logout_redirect_uri: '${baseUrl}/',
   response_type: 'code',
   scope: 'openid email',
 };
